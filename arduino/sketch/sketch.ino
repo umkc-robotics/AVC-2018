2005405cef15a1aa2471e3c1dcf64f5b9b909e72
Kosinkadink <kosinkadink1@gmail.com>
2017-11-12 00:37:19 -0600

    added libraries for use with oled display

--- arduino/sketch/sketch.ino
+++ arduino/sketch/sketch.ino
@@ -1,22 +1,51 @@
+#include <SPI.h>
+#include <Wire.h>
 #include "StartButton.h"
+#include "Adafruit_GFX.h"
+#include "Adafruit_SSD1306.h"
 
 #define START_BUTTON_PIN 2
 
-
-
+// LCD DEFINITION
+Adafruit_SSD1306 display(-1);
+// BUTTON DEFINITION
 StartButton button;
 
 void setup() {
 	button = StartButton(START_BUTTON_PIN);
-	attachInterrupt(digitalPinToInterrupt(START_BUTTON_PIN),buttonFunction,CHANGE);
+	initializeDisplay();
+	initializeButton();
 	Serial.begin(9600);
 }
 
 void loop() {
 	if (button.wasPressed()) {
 		Serial.println("GOOD PRESSED!");
+		showText("PRESSED!");
 		button.reset();
+		delay(200);
+		showText("");
 	}
+}
+
+void initializeDisplay() {
+	display.begin(SSD1306_SWITCHCAPVCC, 0x3C);
+	display.display();
+	display.clearDisplay();
+}
+
+// display text on OLED display
+void showText(String text) {
+	display.setTextSize(1);
+	display.setTextColor(WHITE);
+	display.setCursor(0,0);
+	display.println(text);
+	display.display();
+	display.clearDisplay();
+}
+
+void initializeButton() {
+	attachInterrupt(digitalPinToInterrupt(START_BUTTON_PIN),buttonFunction,CHANGE);
 }
 
 void buttonFunction() {

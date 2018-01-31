1123700b975353091446838d527dc67d90452de0
kosinkadink <kosinkadink1@gmail.com>
2018-01-30 21:11:42 -0600

    added empty files to fill (Sound,Vision,generate_nodes)

---
+++ generate_nodes.py
@@ -0,0 +1,11 @@
+from avc.ConfigReader import ConfigReader
+from avc.GPS import GPS, GPS_Exception
+
+# connect to GPS (connect + wait for fix)
+# while not exited, print latest GPS coordinate
+# if s is entered, current GPS coordinate is added to list
+# if c is entered, a new nodes.json file is created -> displays chart showing coordinates
+
+
+if __name__ == "__main__":
+	pass
